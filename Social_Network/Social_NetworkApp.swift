2d3e93c904371f11952b7665d60b66330b5fb16a
nhutHao02 <trannhuthao091@gmail.com>
2024-03-26 10:55:51 +0700

    Initial Commit

---
+++ Social_Network/Social_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_NetworkApp.swift
+//  Social_Network
+//
+//  Created by TranHao on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Social_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
